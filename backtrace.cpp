f899e4cb0bca8e11eff4147ea0b17a1d99d1f409
Jeremy Agostino <jeremy@groundctl.com>
2017-07-24 13:45:01 -0400

    Fixed the win32 backtrace_symbols() stub to match the declaration on macOS.

--- backtrace.cpp
+++ backtrace.cpp
@@ -18,7 +18,7 @@
 #ifdef __WIN32__
 // We don't have platform support for backtrace() on win32
 static inline int backtrace(void** buffer, int size) { return 0; }
-static inline char** backtrace_symbols(void* const buffer, int size) { return NULL; }
+static inline char** backtrace_symbols(void* const* buffer, int size) { return NULL; }
 #endif
 
 std::vector<void *> _gc_dispatch_get_backtrace()
